9e5a5618dab24b5465bb6984d1424e6381b29783
Benoit Baumann <baumann.benoit@gmail.com>
2019-11-04 13:15:25 +0100

    first version

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    question = params[:question].strip.downcase
+    @answer = if question[-1] == '?'
+                'Silly question, get back to work'
+              elsif question == 'i am going to work'
+                'Great!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
